c82ea46a29c72d6efc736908cd44f322f2649a86
Prince Okwubali <131376077+lovetechcyber@users.noreply.github.com>
2025-05-16 12:40:26 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -9,43 +10,24 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="font-sans text-gray-800 dark:text-white dark:bg-gray-900 scroll-smooth">
-      <Navbar />
-
-<div className="bg-blue-500 text-white p-10">Test Tailwind Background</div>
-      
-      <section id="home">
-        <Hero />
-      </section>
-
-      <section id="about">
-        <About />
-      </section>
-
-      <section id="skills">
-        <Skills />
-      </section>
-
-      <section id="projects">
-        <Projects />
-      </section>
-
-      <section id="experience">
-        <Experience />
-      </section>
-
-      <section id="certifications">
-        <Certifications />
-      </section>
-
-      <section id="contact">
-        <Contact />
-      </section>
-
-      <Footer />
-    </div>
-  );
+return (
+<Router>
+<div className="font-sans text-gray-800 dark:text-white dark:bg-gray-900">
+<Navbar />
+<Routes>
+<Route path="/" element={<Hero />} />
+<Route path="/about" element={<About />} />
+<Route path="/skills" element={<Skills />} />
+<Route path="/projects" element={<Projects />} />
+<Route path="/experience" element={<Experience />} />
+<Route path="/certifications" element={<Certifications />} />
+<Route path="/contact" element={<Contact />} />
+</Routes>
+<Footer />
+</div>
+</Router>
+);
 }
-
 export default App;
+Remove route and make it a one page site with fixed and scroll animation
+
